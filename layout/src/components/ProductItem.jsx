84b5d5fa16e6fb047cee7081dcae86ca89b54cf0
KoltIP <ilya.kolt@gmail.com>
2022-12-09 15:39:59 +0300

    element

--- layout/src/components/ProductItem.jsx
+++ layout/src/components/ProductItem.jsx
@@ -7,11 +7,11 @@ const ProductsList = (props) => {
 
     return( 
         <div className='item'>
-            <img width='200' hieght='150' src={item.img} alt={item.name}></img>
-            <span className='name'>{item.name}</span>            
-            <span>{item.color}</span>
-            <span className='price'>${item.price}</span>
-            <span><button className='button'>ADD TO CART</button></span>        
+            <img width='200' hieght='150' src={item.img} alt={item.name} className='elem'></img>
+            <span className='name elem'>{item.name}</span>            
+            <span className='elem'>{item.color}</span>
+            <span className='price elem'>${item.price}</span>
+            <span><button className='button elem'>ADD TO CART</button></span>        
         </div>
     )    
 }
